2d9859d637912bf71fd666b2e150025cc9036cac
Rahul Verma <rahulvermaxxl@gmail.com>
2020-05-08 00:19:33 +0530

    Initial Commit

--- art_station/art_station/settings.py
+++ art_station/art_station/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp.apps.MainappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'art_station.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'art_station',
+        'USER': 'scott',
+        'PASSWORD': 'tiger',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
